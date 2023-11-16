715164301daee2e08ad32075fddb52123b675559
Ryan Kenndy Hecht <rhecht@uwo.ca>
2023-11-16 16:52:07 -0500

    Added colour constants

--- src/main/java/ca/uwo/cs2212/group2/constants/ColourConstant.java
+++ src/main/java/ca/uwo/cs2212/group2/constants/ColourConstant.java
@@ -3,6 +3,6 @@ package ca.uwo.cs2212.group2.constants;
 import java.awt.Color;
 
 public class ColourConstant {
-    public static final Color PRIMARY_COLOUR = new Color(0x993399);
-    public static final Color BEIGE_COLOUR = new Color(0xEBEBE0);
+  public static final Color PRIMARY_COLOUR = new Color(0x993399);
+  public static final Color BEIGE_COLOUR = new Color(0xEBEBE0);
 }
