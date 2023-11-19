b3ae84475ba2356721a976ffa1d6e0df75c594e8
Ryan Kenndy Hecht <rhecht@uwo.ca>
2023-11-19 13:37:21 -0500

    Added lifecycle hooks to the main class

--- src/main/java/ca/uwo/cs2212/group2/Main.java
+++ src/main/java/ca/uwo/cs2212/group2/Main.java
@@ -1,7 +1,25 @@
 package ca.uwo.cs2212.group2;
 
+/**
+ * This class is the entry point for the application. It contains the main method that is executed
+ * when the application is started. It is responsible for bootstrapping the application and
+ * initializing and cleaning up resources.
+ */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Running main method in Main.java");
+    init();
+    Runtime.getRuntime().addShutdownHook(new Thread(Main::destroy));
+  }
+
+  /** Lifecycle hook to perform initialization when the application is starting up. */
+  private static void init() {
+    // Load configurations
+    // Set up logging
+    // Initialize MVC components
+  }
+
+  /** Lifecycle hook to perform cleanup when the application is shutting down. */
+  private static void destroy() {
+    // Perform any cleanup tasks
   }
 }
