d2ae8612db199c0a0feba5e8dbcb93892b9e0e88
Ryan Kenndy Hecht <rhecht@uwo.ca>
2023-11-19 13:29:09 -0500

    Refactored a bit, documented some classes and added unit tests for TextProcessor

---
+++ src/main/java/ca/uwo/cs2212/group2/constants/ModelConstants.java
@@ -0,0 +1,12 @@
+package ca.uwo.cs2212.group2.constants;
+
+/** This class contains constants used in the model package. */
+public class ModelConstants {
+  /** The maximum number of correction suggestions allowed. */
+  public static final int MAX_NUMBER_OF_CORRECTION_SUGGESTIONS = 4;
+
+  /** Private constructor to prevent instantiation */
+  private ModelConstants() {
+    throw new UnsupportedOperationException("This is a constants class and cannot be instantiated");
+  }
+}
--- src/test/java/ca/uwo/cs2212/group2/model/CorrectionSuggestionsTest.java
+++ src/test/java/ca/uwo/cs2212/group2/model/CorrectionSuggestionsTest.java
@@ -1,27 +1,23 @@
-import ca.uwo.cs2212.group2.model.CorrectionSuggestions;
+package ca.uwo.cs2212.group2.model;
 
-import java.beans.Transient;
-
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 
-
+/** Unit tests for CorrectionSuggestions class */
 public class CorrectionSuggestionsTest {
-    @Test 
-    public void testWord(){
-        CorrectionSuggestions cs = new CorrectionSuggestions("star",3);
-        Assertions.assertEquals("star",cs.getWord());
-        cs.setWord("run");
-        Assertions.assertEquals("run",cs.getWord());
-    }
+  @Test
+  public void testWord() {
+    CorrectionSuggestions cs = new CorrectionSuggestions("star", 3);
+    Assertions.assertEquals("star", cs.getWord());
+    cs.setWord("run");
+    Assertions.assertEquals("run", cs.getWord());
+  }
 
-    @Test
-    public void testDistance(){
-        CorrectionSuggestions cs = new CorrectionSuggestions("star",3);
-        Assertions.assertEquals(3,cs.getDistance());
-        cs.setDistance(5);
-        Assertions.assertEquals(5,cs.getDistance());
-    }
+  @Test
+  public void testDistance() {
+    CorrectionSuggestions cs = new CorrectionSuggestions("star", 3);
+    Assertions.assertEquals(3, cs.getDistance());
+    cs.setDistance(5);
+    Assertions.assertEquals(5, cs.getDistance());
+  }
 }
