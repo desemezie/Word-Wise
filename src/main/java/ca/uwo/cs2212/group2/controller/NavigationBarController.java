7005c798949256d1ddd0772879b8ca3813571076
Ryan Kenndy Hecht <rhecht@uwo.ca>
2023-11-23 18:33:59 -0500

    Working

--- src/main/java/ca/uwo/cs2212/group2/controller/NavigationBarController.java
+++ src/main/java/ca/uwo/cs2212/group2/controller/NavigationBarController.java
@@ -1,15 +1,5 @@
 package ca.uwo.cs2212.group2.controller;
 
-public interface NavigationBarController {
-  void handleFileAction(String actionCommand);
+public class NavigationBarController {
 
-  void handleSettingAction(String actionCommand);
-
-  void handleSpellCheckAction();
-
-  void handleMetricsAction(String actionCommand);
-
-  void handleSaveAction();
-
-  void handleHelpAction(String actionCommand);
 }
