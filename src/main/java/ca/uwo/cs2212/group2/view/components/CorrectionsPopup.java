5d13d3d824dac281fbd5c89a21e3643d3c2720ea
Shaylan Jade Pratt <spratt28@uwo.ca>
2023-11-24 16:17:59 -0500

    made popups

---
+++ src/main/java/ca/uwo/cs2212/group2/view/components/CorrectionsPopup.java
@@ -0,0 +1,64 @@
+package main.java.ca.uwo.cs2212.group2.view.components;
+
+import javax.swing.*;
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CorrectionsPopup extends JDialog {
+
+    private static final String MESSAGE_TEXT = "<html><font color = 'white'>Number of Corrections: 0</font></html>";
+
+    /**
+   * Constructor for the corrections popup
+   *
+   * @param parentFrame the frame to display the popup in
+   */
+    
+        public CorrectionsPopup(JFrame parentFrame){
+            super(parentFrame, "Corrections", true); // 'true' for modal
+    
+            // Set up the content panel
+            JPanel contentPanel = new JPanel();
+            contentPanel.setBackground(new Color(0x993399));
+            contentPanel.setPreferredSize(new Dimension(400, 350));
+            contentPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    
+            // Set up the label with the help message
+            JLabel helpLabel = new JLabel(MESSAGE_TEXT);
+            helpLabel.setPreferredSize(new Dimension(250, 250));
+            contentPanel.add(helpLabel);
+    
+            // Set up the close button
+            JButton closeButton = new JButton("Close");
+            closeButton.addActionListener(
+            new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                CorrectionsPopup.this.dispose(); // Closes the popup
+              }
+            });
+            contentPanel.add(closeButton);
+    
+            // Add content panel to the dialog
+            this.add(contentPanel);
+            this.pack();
+            this.setLocationRelativeTo(parentFrame); // Center relative to the parent frame
+    
+        }
+    
+    
+        /**
+       * Method to display the popup
+       *
+       * @param parentFrame the frame to display the popup in
+       */
+        public static void showCorrectionsDialog(JFrame parentFrame) {
+                CorrectionsPopup popup = new CorrectionsPopup(parentFrame);
+                popup.setVisible(true);
+        }
+        
+    
+}
